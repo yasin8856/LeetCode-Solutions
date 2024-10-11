abfc99b1ba25e0739e546f9cda62bd38eb8999ce
Yasin Şen <118392628+yasin8856@users.noreply.github.com>
2024-10-11 12:53:54 +0300

    Time: 680 ms (7.7%), Space: 16.6 MB (5.29%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,40 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        k = 1
+        while (k * (k + 1)) // 2 <= n:
+            k += 1
+        return k - 1
+
+
+
+
+
+
+
+
+        """
+        m = 0
+        for i in range(1, n + 1):  # 1'den başlıyoruz çünkü ilk sıra 1 bozuk para
+            m += i  # Kullanılan bozuk para sayısı
+            if m == n:  # Tamamlanmış bir sıra
+                return i
+            elif m > n:  # Eğer kalan bozuk paralar tam bir sıra oluşturmuyorsa
+                return i - 1
+        
+        return 0  # Eğer n = 0 olursa (bozuk para yok)
+    """
+
+
+'''
+k = 1
+while (k * (k + 1)) // 2 <= n:
+    k += 1
+return k - 1
+
+counter, x = 0, 1
+    while n>=x:
+        n-=x
+        counter+=1
+        x+=1
+    return counter
+'''
