5809482341eae7972617442c566b55cab57a2daa
Yasin Şen <118392628+yasin8856@users.noreply.github.com>
2024-10-12 19:44:32 +0300

    Time: 85 ms (79.53%), Space: 16.9 MB (13.54%) - LeetHub

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/2112-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/2112-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -4,8 +4,8 @@ class Solution:
             return 0
         nums.sort() #aradaki farkı en aza indirmek için sıralıyoruz
         min_diff = max(nums)
-        
+
         for i in range(len(nums) - k +1):
-            curr_diff = abs(nums[i + k -1] - nums[i])
+            curr_diff = nums[i + k -1] - nums[i]
             min_diff = min(min_diff, curr_diff)
         return min_diff 
