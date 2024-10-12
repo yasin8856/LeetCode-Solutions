bdde00110cb39ab202a12631943aac6008ad3524
Yasin Şen <118392628+yasin8856@users.noreply.github.com>
2024-10-12 19:44:09 +0300

    Time: 99 ms (11.9%), Space: 16.7 MB (52.07%) - LeetHub

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/2112-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/2112-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -1,18 +1,11 @@
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
-        # Eğer k, 1 ise tüm elemanlar aynı olduğu için fark sıfırdır
         if k == 1:
             return 0
+        nums.sort() #aradaki farkı en aza indirmek için sıralıyoruz
+        min_diff = max(nums)
         
-        # Listeyi sıralıyoruz
-        nums.sort()
-
-        # Başlangıçta minimum farkı büyük bir değerle başlatıyoruz
-        min_diff = float('inf')
-
-        # k boyutlu alt gruplar oluşturuyoruz ve en büyük-en küçük farkını buluyoruz
-        for i in range(len(nums) - k + 1):
-            curr_diff = nums[i + k - 1] - nums[i]
+        for i in range(len(nums) - k +1):
+            curr_diff = abs(nums[i + k -1] - nums[i])
             min_diff = min(min_diff, curr_diff)
-        
-        return min_diff
+        return min_diff 
