a29ea34200dd36eb071f803e91468959aee4dba4
Yasin Şen <118392628+yasin8856@users.noreply.github.com>
2024-10-21 15:22:16 +0300

    Time: 2 ms (99.34%), Space: 16.8 MB (10.28%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,22 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()  # Sonucu taşıyacak bir dummy node
-        current = dummy  # Sonucu oluşturmak için pointer
-        carry = 0  # Elde varsa onu tutacak
+        dummy = ListNode()  
+        current = dummy
+        carry = 0
         
-        while l1 or l2 or carry:  # Listeler bitene ve elde kalmayana kadar devam
-            val1 = l1.val if l1 else 0  # l1'in değeri yoksa 0 al
-            val2 = l2.val if l2 else 0  # l2'nin değeri yoksa 0 al
-            summ = val1 + val2 + carry  # Toplam
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            summ = val1 + val2 + carry
             
-            carry = summ // 10  # Elde
-            current.next = ListNode(summ % 10)  # Yeni düğüm ekle
-            current = current.next  # İlerle
+            carry = summ // 10
+            current.next = ListNode(summ % 10)
+            current = current.next
             
             if l1:
                 l1 = l1.next
             if l2:
                 l2 = l2.next
         
-        return dummy.next  # Sonuç dummy'den itibaren döndürülecek
+        return dummy.next 
